ccbb1aced26e82faede703778be09ff65a719254
Pablo Assis <92928619+oGhostWarrior@users.noreply.github.com>
2024-05-16 11:39:50 -0300

    Update code.js

--- code.js
+++ code.js
@@ -13,19 +13,23 @@ function converterDinheiro(){
   document.querySelector("#loading").style.display = 'block';
   
   fetch(`https://economia.awesomeapi.com.br/last/${tipoMoeda}`, {method:'get'})
-  // Aqui eu puxo a api e no fim do link uso "${}" para chamar tipoMoeda e completar o link //
+  // Aqui eu puxo a api e no fim do link uso "${tipoMoeda}" para chamar tipoMoeda na var acima e completar o link, lembrando que tipoMoeda possui os valores do mySelect no HTML//
+  // Entao o Link é completado desta forma: "...com.br/last/BRL-USD" ou "...com.br/last/BTC-BRL" assim por diante.//
     .then(function(response) {
     
         setTimeout(() => {
           
-          response.json().then(function(data) { // Resposta da API//
+          response.json().then(function(data) { 
+            //Resposta da API//
             tipoMoeda = tipoMoeda.replace("-","") 
-            // ^ Aqui é o pulo do gato, ao chamar tipoMoeda estou substituindo o traço "-" de value na HTML, ex: BRL-USD por BRLUSD //
+            // ^ Aqui é o pulo do gato, ao chamar tipoMoeda estou substituindo o traço "-" de value, ex: BRL-USD por BRLUSD //
+            // Isso é necessario pois a API trabalha entregando o valor de data que estamos consultando desta forma//
             let cotacaoMoeda = parseFloat(data[tipoMoeda]["bid"]);
-            // Dessa forma quando chamo os dados da API (data.BRLUSD.bid) o tipoMoeda sera sempre o atual, chamando os dados da moeda selecionada //
+            // Dessa forma quando chamo os dados da API (data.BRLUSD.bid) o tipoMoeda sera sempre o atual, chamando os dados da moeda selecionada//
             let real = campoReal.value;
             let valorConvertido = 0
             if (tipoMoeda == "BTCBRL"){
+              //Essa condição if existe pois a API trabalha com BitCoin em apenas uma direção, assim como estamos convertendo Real paa BitCoin é necessario dividir ao inves de multiplicar.//
               valorConvertido = (real / cotacaoMoeda).toFixed(5);
             }
             else{
@@ -35,7 +39,7 @@ function converterDinheiro(){
                         
            document.querySelector("#loading").style.display = 'none';
            document.querySelector("#result").style.display = 'block';
-
+            //Elementos de estilo, ao clicar em converter acontece um pequeno delay e a tela é bloqueada por um breve momento//
           });
         }, 200)
         
@@ -47,4 +51,4 @@ function converterDinheiro(){
 }
 
 document.querySelector(".convertButton").addEventListener("click", converterDinheiro);
-//campoReal.addEventListener("input", converterDinheiro);
+//Essa parte do codigo demonstra como o JS manipula o HTML, mesmo sem chamada de função no codigo do HTML é possivel criar uma chamada de função para o mesmo.
